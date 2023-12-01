f73bf0907940ab3703db343fff28b4958de9de44
as8ix <abdallaashir67@gmail.com>
2023-12-01 21:33:12 +0300

    tafasir api log

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -11,18 +11,46 @@ async function displaySurah(suwarh){
     
     const surahNames = data.suwar
     
-    console.log(data);
     console.log(surahNames);
 
     surahNames.forEach(suwarh => 
         displaySurah.innerHTML += 
         `<button type="button" class="btn btn-primary col-3 mx-2 my-2" style="width: 250px"
-        value=${suwarh.id}>${suwarh.name}</button>`
+        value=${suwarh.id}>سورة ${suwarh.name}</button>`
         );
-        console.log(suwarh.name)
+        // console.log(suwarh.name)
     
 
 }
 displaySurah()
 
 
+// al tafasir
+async function playTafasir(){
+
+    const audioPlayer = document.querySelector("#audioPlayer")
+
+    const res = await fetch(`https://mp3quran.net/api/v3/tafsir`)
+    const data = await res.json()
+
+    const tafasir = data.tafasir.soar;
+
+
+    console.log(tafasir);
+
+
+
+
+
+
+
+
+
+    // audioPlayer.addEventListener('change', e => {
+    //     const selectedSurah = audioPlayer.options[audioPlayer.selectedIndex]
+    //     playSurah(selectedSurah.value)
+
+    // })
+
+}
+playTafasir()

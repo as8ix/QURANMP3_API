445a5554e54d5a685d98faa17b58cda501d118e4
as8ix <abdallaashir67@gmail.com>
2023-12-05 17:33:13 +0300

    last touches on the tafsir part

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -6,28 +6,19 @@ async function displaySuwar(){
     const displaySurah = document.querySelector("#displaySurah")
     const res = await fetch(`${apiUrl}/tafsir`)
     const data = await res.json()
-    
-    
-    const suraName = data.tafasir.soar[0].name
-    const tafasir = data.tafasir.soar[0].url
-    const tafasiirId = data.tafasir.soar[0].sura_id
-    
-    console.log(tafasir, suraName, tafasiirId ,data.tafasir.soar[0]);
+    const tafasir = data.tafasir.soar
 
-    data.tafasir.soar.forEach(suwarh =>
-    displaySurah.innerHTML += `<button type="button" class="btn btn-primary col-3 mx-2 my-2" style="width: 250px"
-        value=${tafasir.sura_id}> تفسير ${tafasir}</button>`);
+    data.tafasir.soar.forEach((item) =>
+    displaySurah.innerHTML += `<button onclick="playAudio(this.value)" type="button" class="btn btn-primary col-3 mx-2 my-2" style="width: 250px"
+        value=${item.url}> تفسير ${item.name}</button>`);
         
-   
-        
-
-    // surahNames.forEach(suwarh => 
-    //     displaySurah.innerHTML += 
-    //     `<button type="button" class="btn btn-primary col-3 mx-2 my-2" style="width: 250px"
-    //     value=${suwarh.id}>سورة ${suwarh.name}</button>`
-    //     );
-    //     // console.log(suwarh.name)
     
 
 }
-displaySuwar();
+function playAudio(audioSrc) {
+    var audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.src = audioSrc;
+    audioPlayer.play();
+  }
+  playAudio()
+  displaySuwar();

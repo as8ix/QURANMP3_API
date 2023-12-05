4887c6f27ebedae719223f29128ca77a338a8e66
as8ix <abdallaashir67@gmail.com>
2023-12-05 21:04:25 +0300

    finished everything

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -21,3 +21,15 @@ function playAudio(audioSrc) {
     audioPlayer.play();
   }
   playAudio()
+
+  function playLive(channel){
+    if(Hls.isSupported()) {
+        var video = document.getElementById('liveVideo');
+        var hls = new Hls();
+        hls.loadSource(`${channel}`);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+          video.play();
+        });
+    }
+}

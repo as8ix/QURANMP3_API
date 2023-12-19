e863e45c506cca4e451f04a2bf09517ed858de1c
as8ix <abdallaashir67@gmail.com>
2023-12-19 18:18:55 +0300

    touches

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -6,10 +6,11 @@ async function displaySuwar(){
     const displaySurah = document.querySelector("#displaySurah")
     const res = await fetch(`${apiUrl}/tafsir`)
     const data = await res.json()
+    // console.log(data);
     const tafasir = data.tafasir.soar
 
     data.tafasir.soar.forEach((item) =>
-    displaySurah.innerHTML += `<button onclick="playAudio(this.value)" type="button" class="btn btn-primary col-3 mx-2 my-2" style="width: 250px"
+    displaySurah.innerHTML += `<button onclick="playAudio(this.value)" type="button" class="btn btn-primary col-3 mx-2 my-2" style="width: 280px;text-shadow: 2px 1px 5px #000;"
         value=${item.url}> تفسير ${item.name}</button>`);
         
 
